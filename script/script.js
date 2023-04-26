c77568ed64c049607764bbd5ed7611dd10031d3d
Metwet <tselm.matv@gmail.com>
2023-04-27 00:21:19 +0400

    mobile menu click

--- script/script.js
+++ script/script.js
@@ -1,19 +1,34 @@
 const filmreel = document.querySelector('.filmreel');
 const menu = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menu_item');
 const wheel = document.querySelector('.wheel');
 let isMenuOpen = false;
 let rotateAngle = 0;
 
+function closeMenu() {
+    menu.style.top = "-455px";
+    isMenuOpen = false;
+    rotateAngle -= 360;
+    wheel.style.transform = `rotate(${rotateAngle}deg)`;
+}
+
+function openMenu() {
+    menu.style.top = "0";
+    isMenuOpen = true;
+    rotateAngle += 360;
+    wheel.style.transform = `rotate(${rotateAngle}deg)`;
+}
+
 filmreel.addEventListener('click', () => {
     if(isMenuOpen){
-        menu.style.top = "-455px";
-        isMenuOpen = false;
-        rotateAngle -= 360;
-        wheel.style.transform = `rotate(${rotateAngle}deg)`;
+        closeMenu();
     } else {
-        menu.style.top = "0";
-        isMenuOpen = true;
-        rotateAngle += 360;
-        wheel.style.transform = `rotate(${rotateAngle}deg)`;
+        openMenu();
     }
 });
+
+for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', () => {
+        closeMenu();
+    });
+}
